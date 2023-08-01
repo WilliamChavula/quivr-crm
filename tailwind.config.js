507669c85596dca7f63d2acdb1a1849cdb82974e
williamchavula <rumbani@gmail.com>
2023-08-01 09:44:27 -0500

    configured tailwind in the django project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}", "./templates/**/*.html", "./**/templates/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
