0c33a7ba214123fd3b0c8f916b09e1699ddd4b8d
williamchavula <rumbani@gmail.com>
2023-08-02 10:45:20 -0500

    implemented CRUD operations for leads.

--- lead/urls.py
+++ lead/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = "lead"
 
 urlpatterns = [
-    path("", views.ListLeadView.as_view(), name="list"),
-    path("add/", views.CreateLeadView.as_view(), name="create"),
-    path("detail/<int:pk>/", views.LeadDetailView.as_view(), name="detail"),
+    path("", views.LeadListView.as_view(), name="list"),
+    path("add/", views.LeadCreateView.as_view(), name="create"),
+    path("<int:pk>/detail/", views.LeadDetailView.as_view(), name="detail"),
+    path("<int:pk>/delete/", views.LeadDeleteView.as_view(), name="delete"),
+    path("<int:pk>/update/", views.LeadUpdateView.as_view(), name="update"),
 ]
--- lead/views.py
+++ lead/views.py
@@ -1,27 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
+
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import FormView, ListView, DetailView
+from django.views.generic import DetailView, FormView, ListView, UpdateView, View
 
 from .forms import LeadCreateForm
 from .models import Lead
 
 
-class CreateLeadView(LoginRequiredMixin, FormView):
+class LeadCreateView(LoginRequiredMixin, SuccessMessageMixin, FormView):
     template_name = "lead/create.html"
     form_class = LeadCreateForm
     redirect_authenticated_user = True
-    success_url = reverse_lazy("dashboard:board")
+    success_url = reverse_lazy("lead:list")
+    success_message = "Lead created successfully"
 
     def form_valid(self, form):
         lead = form.save(commit=False)
         lead.created_by = self.request.user
         lead.save()
 
-        return super(CreateLeadView, self).form_valid(form)
+        return super(LeadCreateView, self).form_valid(form)
 
 
-class ListLeadView(LoginRequiredMixin, ListView):
+class LeadListView(LoginRequiredMixin, ListView):
     model = Lead
     context_object_name = "leads"
     template_name = "lead/list.html"
@@ -39,7 +43,29 @@ class LeadDetailView(LoginRequiredMixin, DetailView):
     template_name = "lead/detail.html"
 
     def get_queryset(self):
-        # original qs
         qs = super().get_queryset()
-        # filter by a variable captured from url, for example
         return qs.filter(created_by=self.request.user)
+
+
+class LeadUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Lead
+    template_name = "lead/update.html"
+    form_class = LeadCreateForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("lead:list")
+    success_message = "Lead changes saved successfully"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(created_by=self.request.user)
+
+
+class LeadDeleteView(LoginRequiredMixin, View):
+    def get(self, request, pk: int, *args, **kwargs):
+        lead: Lead = get_object_or_404(Lead, pk=pk)
+
+        lead.delete()
+
+        messages.success(request, "Lead successfully deleted.")
+
+        return redirect("lead:list")
