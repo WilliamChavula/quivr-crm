e7e7ac31ef2fe2a96855b72c8c8292d4fd21fc63
williamchavula <rumbani@gmail.com>
2023-08-02 13:04:56 -0500

    implement displaying clients

--- client/urls.py
+++ client/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = "client"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.ClientListView.as_view(), name="list"),
+]
--- client/views.py
+++ client/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Client
+
+
+class ClientListView(LoginRequiredMixin, ListView):
+    model = Client
+    template_name = "client/list.html"
+    context_object_name = "clients"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(created_by=self.request.user)
